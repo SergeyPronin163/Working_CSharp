f7f384a1ff289b08d4e2b5396bad22a28a540e21
Pronin Sergey <Prizrak163l@mail.ru>
2024-02-21 15:29:02 +0400

    Add dotnet

---
+++ Seminar1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+
+
+int first = 20;
+int second = 5;
+
+if (second * second == first)
+{
+    Console.WriteLine("Yes");
+}
+else{
+    Console.WriteLine("No");
+}
