a1f31cf11efa5653aa88c047d9bd2ab58556d116
Pronin Sergey <Prizrak163l@mail.ru>
2024-02-21 15:55:48 +0400

    Add Task3

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное целое число и на 
+// выходе показывает сумму первой и последней цифры этого числа.
+
+int num = 123;
+int first = num / 100;
+int last = num % 10;
+int sum = first + last;
+Console.Write("Sum of first and last digit is - " + sum);
