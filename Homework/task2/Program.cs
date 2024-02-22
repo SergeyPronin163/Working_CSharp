66b7787ec4d6077d74427c507bb176d157c01723
Pronin Sergey <Prizrak163l@mail.ru>
2024-02-22 21:58:19 +0400

    add homework:task2

---
+++ Homework/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдает макимальное из этих чисел
+
+int a = 5;
+    int b = 6;
+    int c = 7;
+    int FindMax = a;
+      
+    if (b > a)
+    {
+      FindMax = b;
+    }
+    if (c > b)
+    {
+      FindMax = c;
+    }
+    Console.WriteLine(FindMax);
