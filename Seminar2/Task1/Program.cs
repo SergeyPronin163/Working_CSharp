19a775f9a5b7cc869163ac012a3bdf36843f3052
Pronin Sergey <Prizrak163l@mail.ru>
2024-02-23 23:30:19 +0400

    Add Seminar2:Task1

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите пограмму, которая принимает на вход
+// трехзначное число и удаляет вторую цифу этого числа.
+
+int num = 123;
+int first = num/100;
+int second = num%10;
+Console.Write("Numb without second dight is - " + first+second);
